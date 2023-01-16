1be9d4e77867eb0f87e8e133fc2b948c15173eb2
Carter Small <csmall@student.neumont.edu>
2023-01-15 17:59:40 -0700

    AI -Flocking

---
+++ Assets/Scripts/Automas Agent/AutonomousAgent.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor.EditorTools;
+using UnityEngine;
+
+public class AutonomousAgent : Agent
+{
+    public Perception flockPerception;
+    public AutonomousAgentData data;
+
+    public float wanderAngle { get; set; } = 0;
+
+
+    void Update()
+    {
+        var gameObjects = perception.GetGameObjects();
+
+
+
+        if (gameObjects.Length > 0)
+        {
+            movement.ApplyForce(Steering.Seek(this, gameObjects[0]) * data.seekWeight);
+            movement.ApplyForce(Steering.Flee(this, gameObjects[0]) * data.fleeWeight);
+
+        }
+
+        gameObjects = flockPerception.GetGameObjects();
+        if (gameObjects.Length > 0)
+        {
+            foreach (var gameObject in gameObjects)
+            {
+                Debug.DrawLine(transform.position, gameObject.transform.position);
+            }
+            movement.ApplyForce(Steering.Cohesion(this, gameObjects) * data.cohesionWeight);
+            movement.ApplyForce(Steering.Separation(this, gameObjects, data.separationRadius) * data.separationWeight);
+            movement.ApplyForce(Steering.Alignment(this, gameObjects) * data.alignmentWeight);
+        }
+
+
+        if (movement.acceleration.sqrMagnitude <= movement.maxForce * 0.1f)
+        {
+            movement.ApplyForce(Steering.Wander(this));
+        }
+
+        transform.position = Utilities.Wrap(transform.position, new Vector3(-20, -20, -20), new Vector3(20, 20, 20));
+
+    }
+}
